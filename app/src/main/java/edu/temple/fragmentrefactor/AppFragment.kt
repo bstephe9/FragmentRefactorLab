95a81d179f369b04e0c0fbd6229b18005977a799
Ben Stephenson <tun78906@temple.edu>
2024-03-20 15:48:08 -0400

    feat: Add functionality to fragment instead of activity

--- app/src/main/java/edu/temple/fragmentrefactor/AppFragment.kt
+++ app/src/main/java/edu/temple/fragmentrefactor/AppFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class AppFragment : Fragment() {
 
@@ -13,6 +16,20 @@ class AppFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_app, container, false)
+        val view = inflater.inflate(R.layout.fragment_app, container, false)
+
+        val displayTextView = view.findViewById<TextView>(R.id.displayTextView)
+        val nameEditText = view.findViewById<EditText>(R.id.nameEditText)
+        val changeButton = view.findViewById<Button>(R.id.changeButton)
+
+        changeButton.setOnClickListener {
+            val name = nameEditText.text
+
+            displayTextView.text = if (name.isNotBlank()) {
+                "Hello, $name!"
+            } else {
+                "Please enter your name"
+            }
+        }
     }
 }
